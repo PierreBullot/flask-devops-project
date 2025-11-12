987a63d4deae7b32b94089ac6bc727fe78fbd0d0
Muriel Bullot <murielbullot@hotmail.fr>
2025-11-12 16:41:59 +0100

    création de read_videos_json et create_new_id

---
+++ function_json.py
@@ -0,0 +1,24 @@
+# function_json.py
+import os
+import json
+
+def read_videos_json(file_path):
+    """Lit le fichier JSON et renvoie une liste de vidéos."""
+    if os.path.exists(file_path):
+        with open(file_path, 'r', encoding='utf-8') as f:
+            try:
+                videos = json.load(f)                       # lit le contenu du fichier JSON et le convertit en objet Python
+            except json.JSONDecodeError:                    # Le fichier existe mais est vide ou corrompu
+                videos = []                                 # initialise une liste vide
+    else:
+        videos = []                                         # initialise une liste vide
+    return videos
+
+def create_new_id(videos_file):
+    """Crée une nouvelle ID."""
+    new_id = 0                    # valeur par défaut si la liste est vide
+    for v in videos_file:
+        if v['id'] > new_id:
+            new_id = v['id']      # recherche de la plus grande valeur de ID
+    return new_id + 1
+
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
+from function_json import read_videos_json, create_new_id
+import json
+import os
 
 app = Flask(__name__)
+VIDEOS_FILE = "videos.json"
 
 
 @app.route("/")
@@ -13,10 +17,36 @@ def videos():
     video_list = ["test", "placeholder", "simple"]
     return render_template("videos.html", videos=video_list)
 
+@app.route('/videos/add', methods=['POST'])
+def add_video_post():
+    # Récupérer les données du formulaire 
+    title = request.form['title']
+    url = request.form['url']
+
+    # Lire le fichier videos.json
+    videos = read_videos_json(VIDEOS_FILE)
+    
+    # Créer un nouvel ID
+    new_id = create_new_id(videos)
+
+    # créer une nouvelle vidéo
+    new_video = {
+        "id": new_id,
+        "title": title,
+        "url": url,
+        "views": 0,
+    }
+
+    # ajouter la nouvelle vidéo à la liste
+    videos.append(new_video)
+
+    # Ecrire dans le fichier .json
+    with open(VIDEOS_FILE, 'w', encoding='utf-8') as f:
+        json.dump(videos, f, indent=4, ensure_ascii=False)
 
 # Route GET /videos/add
 @app.route('/videos/add', methods=['GET'])          # GET : lecture/affichage
-def add_video():
+def add_video_get():
     return render_template('add_video.html')        # cherche et interprète le fichier add_video.html dans le dossier templates/
 
 if __name__ == '__main__':                          # permet d’exécuter le code à l’intérieur uniquement si ce fichier est lancé directement
