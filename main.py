429d590c7f8d52e1ec515e5cf527697bd7e6d126
Pierre Bullot <pierre-bullot@hotmail.fr>
2025-11-12 11:53:35 +0100

    Page liste de vidéos ajoutée

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello_world():
+def home():
     return render_template("index.html")
+
+
+@app.route("/videos")
+def videos():
+    video_list = ["test", "placeholder", "simple"]
+    return render_template("videos.html", videos=video_list)
