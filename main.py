3a2ae31ea723fb438a3eb7fa66195f9b5155cf86
Muriel Bullot <murielbullot@hotmail.fr>
2025-11-13 15:30:50 +0100

    réunion des get et post  dans une seule fonction

--- main.py
+++ main.py
@@ -36,31 +36,32 @@ def video(target_id):
     return redirect(url_for('videos'))
 
 
-# Route POST /videos/add
-@app.route('/videos/add', methods=['POST'])
-def add_video_post():
-    # Récupérer les données du formulaire 
-    title = request.form['title']
-    url = request.form['url']
-    # Lire le fichier videos.json et renvoie une liste des videos
-    video_list = functions_json.get_video_list(VIDEOS_FILE)
-    print(f"------------------------------------------- première liste : {video_list}")
-    # Créer une nouvelle ID
-    new_id = functions_json.create_new_id(video_list)
-    print(f"le nouvel id sera : {new_id}")
-    # Ajouter la nouvelle vidéo à la liste des vidéos
-    video_list = functions_json.add_video_into_list(video_list, new_id, title, url)
-    print(f"-------------------------------------------  nouvelle liste : {video_list}")
-    # enregistrer la liste des vidéos dans le fichier videos.json 
-    # et afficher le message de réussite
-    print(functions_json.save_video_list(video_list, VIDEOS_FILE))
-    # revenir à la page du formulaire d'ajout
-    return redirect(url_for('add_video_get'))
 
-# Route GET /videos/add
-@app.route('/videos/add', methods=['GET'])          # GET : lecture/affichage
-def add_video_get():
-    return render_template('add_video.html')        # cherche et interprète le fichier add_video.html dans le dossier templates/
+# Route POST/videos/add  et GET//videos/add
+@app.route('/videos/add', methods=["GET", "POST"])
+def add_video():
+    if request.method == "GET":
+        # Récupérer les données du formulaire 
+        title = request.form['title']
+        url = request.form['url']
+        # Lire le fichier videos.json et renvoie une liste des videos
+        video_list = functions_json.get_video_list(VIDEOS_FILE)
+        print(f"------------------------------------------- première liste : {video_list}")
+        # Créer une nouvelle ID
+        new_id = functions_json.create_new_id(video_list)
+        print(f"le nouvel id sera : {new_id}")
+        # Ajouter la nouvelle vidéo à la liste des vidéos
+        video_list = functions_json.add_video_into_list(video_list, new_id, title, url)
+        print(f"-------------------------------------------  nouvelle liste : {video_list}")
+        # enregistrer la liste des vidéos dans le fichier videos.json 
+        # et afficher le message de réussite
+        print(functions_json.save_video_list(video_list, VIDEOS_FILE))
+        # revenir à la page du formulaire d'ajout
+        return redirect(url_for('add_video_get'))
+    
+    if request.method == "POST":
+        return render_template('add_video.html')        # cherche et interprète le fichier add_video.html dans le dossier templates/
+
 
 if __name__ == '__main__':                          # permet d’exécuter le code à l’intérieur uniquement si ce fichier est lancé directement
     app.run(debug=True)                             # Démarre le serveur de développement Flask et active le mode debug : rechargement automatique à chaque modification du code, affichage d’erreurs détaillées si un bug se produit.
