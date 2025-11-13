f43dfcda8a869ddc353fa82e2bd2f585328c152f
Muriel Bullot <murielbullot@hotmail.fr>
2025-11-13 12:39:46 +0100

    l'ajout d'une vidéo dans le fichier .json est OK

--- function_json.py
+++ function_json.py
@@ -5,20 +5,42 @@ import json
 def get_video_list(file_path):
     """Lit le fichier JSON et renvoie une liste de vidéos."""
     if os.path.exists(file_path):
-        with open(file_path, 'r', encoding='utf-8') as f:
+        with open(file_path, 'r', encoding='utf-8') as data:
             try:
-                videos = json.load(f)                       # lit le contenu du fichier JSON et le convertit en objet Python
+                videos = json.load(data)                       # lit le contenu du fichier JSON et le convertit en objet Python
             except json.JSONDecodeError:                    # Le fichier existe mais est vide ou corrompu
                 videos = []                                 # initialise une liste vide
     else:
         videos = []                                         # initialise une liste vide
     return videos
 
-def create_new_id(videos_file):
+def create_new_id(video_list):
     """Crée une nouvelle ID."""
     new_id = 0                    # valeur par défaut si la liste est vide
-    for v in videos_file:
+    for v in video_list:
         if v['id'] > new_id:
             new_id = v['id']      # recherche de la plus grande valeur de ID
     return new_id + 1
 
+
+def save_video_list(video_list, FILE_JSON):
+    """Ecrire la liste des vidéos dans un fichier .json"""
+    with open(FILE_JSON, mode="w", encoding="utf-8") as f:          # ouvre le fichier .json (FILE_JSON) appelé "f", en mode écriture (effacera le contenu précédent)
+        json.dump(video_list, f, indent=4, ensure_ascii=False)      # convertit la liste des vidéos (video_list) en texte JSON, ajoute une identation de 4 pour le formatage,
+    return "Vidéo ajoutée avec succès !"                                                                # garde les caractères accentués sans les convertir en code Unicode et écrit-la dans le fichier appelé "f"
+
+
+def add_video_into_list(video_list, new_id, new_title, new_url):
+    """Ajoute une nouvelle vidéo à la liste des vidéos"""
+    new_video = {
+        "id": new_id,
+        "title": new_title,
+        "url": new_url,
+        "views": 0,
+    }
+    video_list.append(new_video)
+    return video_list
+
+
+
+    
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
-from function_json import get_video_list, create_new_id
+from function_json import get_video_list, create_new_id, save_video_list, add_video_into_list
 import json
 import os
 
@@ -14,7 +14,7 @@ def home():
 
 @app.route("/videos")
 def videos():
-    video_list = ["test", "placeholder", "simple"]
+    video_list = get_video_list(VIDEOS_FILE)
     return render_template("videos.html", videos=video_list)
 
 @app.route('/videos/add', methods=['POST'])
@@ -24,25 +24,20 @@ def add_video_post():
     url = request.form['url']
 
     # Lire le fichier videos.json et renvoie une liste des videos
-    videos = get_video_list(VIDEOS_FILE)
-    
+    video_list = get_video_list(VIDEOS_FILE)
+    print(f"------------------------------------------- première liste : {video_list}")
+
     # Créer une nouvelle ID
-    new_id = create_new_id(videos)
+    new_id = create_new_id(video_list)
+    print(f"le nouvel id sera : {new_id}")
 
-    # créer une nouvelle vidéo
-    new_video = {
-        "id": new_id,
-        "title": title,
-        "url": url,
-        "views": 0,
-    }
+    # Ajouter la nouvelle vidéo à la liste des vidéos
+    video_list = add_video_into_list(video_list, new_id, title, url)
+    print(f"-------------------------------------------  nouvelle liste : {video_list}")
+    # ENregistrer la liste des vidéos dans le fichier videos.json
+    print(save_video_list(video_list, VIDEOS_FILE))
 
-    # ajouter la nouvelle vidéo à la liste
-    videos.append(new_video)
-
-    # Ecrire dans le fichier .json
-    with open(VIDEOS_FILE, 'w', encoding='utf-8') as f:
-        json.dump(videos, f, indent=4, ensure_ascii=False)
+    return redirect(url_for('add_video_get'))
 
 # Route GET /videos/add
 @app.route('/videos/add', methods=['GET'])          # GET : lecture/affichage
