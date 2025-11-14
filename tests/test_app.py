2e7b3c4e79fe68636d9f28989b202f225160e401
Pierre Bullot <pierre-bullot@hotmail.fr>
2025-11-14 11:18:11 +0100

    Test ajouté pour la modification de vidéo

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,26 +6,36 @@ import main
 
 
 def test_home_page():
-    """Test que la page d'accueil fonctionne"""
+    """Teste que la page d'accueil fonctionne."""
     client = main.app.test_client()
+
     response = client.get('/')
     assert response.status_code == 200
 
 
 def test_list_page():
-    """Test que la page d'accueil fonctionne"""
+    """Teste que la page qui affiche la liste des vidéos fonctionne."""
     client = main.app.test_client()
+
     response = client.get('/videos')
     assert response.status_code == 200
 
 
 def test_video_page():
-    """Test que la page d'accueil fonctionne"""
+    """Teste que la page qui affiche une vidéo particulière fonctionne."""
     client = main.app.test_client()
+
     # Vérifier qu'une vidéo existante est bien affichée
     response = client.get('/videos/1')
     assert response.status_code == 200
+
     # Vérifier qu'une vidéo inexistante donne une redirection
     response = client.get('/videos/100')
     assert response.status_code == 302
 
+    # Vérifier que la modification d'une vidéo fonctionne
+    response = client.post('/videos/1', data={
+        "title": "titre test",
+        "modify": "Modifier"
+    })
+    assert response.status_code == 200
